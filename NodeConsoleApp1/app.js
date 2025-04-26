82cf9f8c2e2cc85e75284bb5224eec8ce2ef9da1
Rani Lodha <125969857+rani25lodha@users.noreply.github.com>
2025-04-26 15:33:47 +0530

    Update app.js

--- NodeConsoleApp1/app.js
+++ NodeConsoleApp1/app.js
@@ -9,5 +9,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
